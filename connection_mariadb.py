61c5670501d28e387524b53ee4682ffe02279019
Hafedh573 <90450704+Hafedh573@users.noreply.github.com>
2022-09-14 11:34:44 +0100

    Create connection_mariadb.py

---
+++ connection_mariadb.py
@@ -0,0 +1,14 @@
+import mysql.connector
+#*36342DF49762A4AC4BE08FBD3207679E1D6A16E7
+#An0thrS3crt
+mydb=mysql.connector.connect(host="127.0.0.1",user="citizix_user",passwd="An0thrS3crt")
+print(mydb)
+if (mydb):
+  print("Connection to Mariadb established successfully")
+else:
+  print("Connection to Mariadb failed")
+mycursor=mydb.cursor()
+#mycursor.execute("create database test_bd")
+mycursor.execute("show databases")
+for db in mycursor:
+  print(db)
